7f0eecfda0cf9b48adc45278575b959e71948406
Hung <0950080122@sv.hcmunre.edu.vn>
2023-05-25 23:49:55 +0700

    abc

---
+++ MVCBookStore/Controllers/BookStoreController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcBookStore.Models;
+
+namespace MvcBookStore.Controllers
+{
+    public class BookStoreController : Controller
+    {
+        //Tạo 1 đối tượng chứa toàn bộ CSDL từ dbQLBansach
+        dbQLBansachDataContext data = new dbQLBansachDataContext();
+
+        private List<SACH> Laysachmoi(int count)
+        {
+            //Sắp xếp giảm dần theo Ngaycapphat, lay count dong dau
+            return data.SACHes.OrderByDescending(a => a.Ngaycapnhat).Take(count).ToList();
+        }
+        // GET: BookStore
+        public ActionResult Index()
+        {
+            //lấy 5 quyển sách mới nhất
+            var sachmoi = Laysachmoi(5);
+            return View(sachmoi);
+        }
+        public ActionResult Chude()
+        {
+            var chude = from cd in data.CHUDEs select cd;
+            return PartialView(chude);
+        }
+        public ActionResult Nhaxuatban()
+        {
+            var nhaxuatban = from nxb in data.NHAXUATBANs select nxb;
+            return PartialView(nhaxuatban);
+        }
+        public ActionResult SPTheochude(int id)
+        {
+            var sach = from s in data.SACHes where s.MaCD == id select s;
+            return View(sach);
+        }
+        public ActionResult SPTheoNXB(int id)
+        {
+            var sach = from s in data.SACHes where s.MaNXB == id select s;
+            return View(sach);
+        }
+        public ActionResult Details(int id)
+        {
+            var sach = from s in data.SACHes where s.Masach == id select s;
+            return View(sach.Single());
+        }
+    }
+}
